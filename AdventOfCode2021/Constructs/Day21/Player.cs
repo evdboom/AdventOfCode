e166f85e223d207dfc92f8b81b1babf16fac633b
Erik van der Boom <Erik.van.der.boom@cavero.nl>
2021-12-21 14:55:43 +0100

    Used part 2 processing for part 1 (performance)

--- AdventOfCode2021/Constructs/Day21/Player.cs
+++ AdventOfCode2021/Constructs/Day21/Player.cs
@@ -3,6 +3,14 @@
     public class Player
     {       
         public Dictionary<int, Score> Scores { get; set; } = new();
+        
+        public Player(bool addScore = false)
+        {
+            if (addScore)
+            {
+                Scores[0] = new Score();
+            }
+        }
 
         public long GetWeight()
         {
--- AdventOfCode2021/Days/Day21.cs
+++ AdventOfCode2021/Days/Day21.cs
@@ -6,6 +6,16 @@ namespace AdventOfCode2021.Days
     public class Day21 : Day
     {
         private const string PlayerSplit = " starting position: ";
+        private readonly Dictionary<int, int> _options = new Dictionary<int, int>
+        {
+            { 3, 1 },
+            { 4, 3 },
+            { 5, 6 },
+            { 6, 7 },
+            { 7, 6 },
+            { 8, 3 },
+            { 9, 1 }
+        };
 
         public Day21(IFileImporter importer) : base(importer)
         {
@@ -15,75 +25,70 @@ namespace AdventOfCode2021.Days
 
         protected override long ProcessPartOne(string[] input)
         {
-            var players = input
-                .Select(i => i.Split(PlayerSplit))
-                .ToDictionary(p => p[0], p => (Position: int.Parse(p[1]), Score: 0));
+            var (players1, players2) = GetPlayers(input);
+
             int count = 0;
-            int roll = 0;
-            while (players.All(p => p.Value.Score < 1000))
-            {
-                foreach(var player in players)
+
+            while (players1.Any() && players2.Any())
+            {             
+                count += 3;
+                var next = count % 1000;
+                var turn = (next == 0 ? 1000 : next) * 3 - 3;
+
+                players1 = GetNextRound(players1, out List<long> winningWeights, new Dictionary<int, int> { { turn, 1 } }, 1000);                
+                if (winningWeights.Any())
                 {
-                    var score = 0;
-                    for (int i = 0; i < 3; i ++)
-                    {
-                        count++;
-                        roll++;
-                        if (roll > 1000)
-                        {
-                            roll = 1;
-                        }
-                        score += roll;                        
-                    }
-                    var pos = (player.Value.Position + score) % 10;
-                    if (pos == 0)
-                    {
-                        pos = 10;
-                    }
-                    players[player.Key] = (pos, player.Value.Score + pos);
-                    if (players[player.Key].Score >= 1000)
-                    {
-                        break;
-                    }
-                }                
+                    return players2.Single().Value.Scores.Single().Key * count;
+                }
+
+                count += 3;
+                turn = (count % 1000) * 3 - 3;
+
+                players2 = GetNextRound(players2, out winningWeights, new Dictionary<int, int> { { turn, 1 } }, 1000);
+                if (winningWeights.Any())
+                {
+                    return players1.Single().Value.Scores.Single().Key * count;
+                }
             }
 
-            var other = players.Single(p => p.Value.Score < 1000);
-            return other.Value.Score * count;
+            throw new InvalidOperationException("No winner found");
         }
-
+      
         protected override long ProcessPartTwo(string[] input)
         {
-            var playerPositions = input
-                .Select(i => i.Split(PlayerSplit))
-                .Select(p => int.Parse(p[1]))
-                .ToArray();            
+            var (players1, players2) = GetPlayers(input);
 
-            var players1 = new Dictionary<int, Player>();            
-            var initial1 = new Player();
-            initial1.Scores[0] = new Score();            
-            players1[playerPositions[0]] = initial1;
-            var players2 = new Dictionary<int, Player>();
-            var initial2 = new Player();
-            initial2.Scores[0] = new Score();
-            players2[playerPositions[1]] = initial2;
-            
             long player1Wins = 0;
             long player2Wins = 0;
 
             while (players1.Any() && players2.Any())
             {
-                players1 = GetNextRound(players1, out List<long> winningWeights);
+                players1 = GetNextRound(players1, out List<long> winningWeights, _options, 21);
                 player1Wins += winningWeights.Sum(w => w * players2.Select(p => p.Value.GetWeight()).Sum());
-
-                players2 = GetNextRound(players2, out winningWeights);
+                players2 = GetNextRound(players2, out winningWeights, _options, 21);
                 player2Wins += winningWeights.Sum(w => w * players1.Select(p => p.Value.GetWeight()).Sum());
             }
 
             return Math.Max(player1Wins, player2Wins);
         }
 
-        private Dictionary<int, Player> GetNextRound(Dictionary<int, Player> players, out List<long> winningWeights)
+        private (Dictionary<int,Player> Players1, Dictionary<int,Player> Players2) GetPlayers(string[] input)
+        {
+            
+            var playerPositions = input
+                .Select(i => i.Split(PlayerSplit))
+                .Select(p => int.Parse(p[1]))
+                .ToArray();
+
+            var players1 = new Dictionary<int, Player>();            
+            players1[playerPositions[0]] = new Player(addScore: true);
+            var players2 = new Dictionary<int, Player>();            
+            players2[playerPositions[1]] = new Player(addScore: true);
+
+            return (players1, players2);
+        }
+
+        private Dictionary<int, Player> GetNextRound(Dictionary<int, Player> players, out List<long> winningWeights, Dictionary<int, int> options, int target)
         {
             var nextRound = new Dictionary<int, Player>();
             winningWeights = new();
@@ -91,25 +96,25 @@ namespace AdventOfCode2021.Days
             {
                 foreach (var score in player.Value.Scores)
                 {
-                    foreach (var option in GetOptions())
+                    foreach (var option in options)
                     {
                         var nextPosition = (player.Key + option.Key) % 10;
                         nextPosition = nextPosition == 0 ? 10 : nextPosition;
                         var newScore = score.Key + nextPosition;
                         var newWeight = score.Value.Weight * option.Value;
 
-                        if (newScore >= 21)
+                        if (newScore >= target)
                         {
                             winningWeights.Add(newWeight);
                         }
                         else
                         {
-                            if (!nextRound.TryGetValue(nextPosition, out Player next))
+                            if (!nextRound.TryGetValue(nextPosition, out Player? next))
                             {
                                 next = new Player();
                                 nextRound[nextPosition] = next;
                             }
-                            if (!next.Scores.TryGetValue(newScore, out Score nextScore))
+                            if (!next.Scores.TryGetValue(newScore, out Score? nextScore))
                             {
                                 nextScore = new Score { Weight = newWeight };
                                 next.Scores[newScore] = nextScore;
@@ -124,20 +129,6 @@ namespace AdventOfCode2021.Days
             }
 
             return nextRound;
-        }
-  
-        private Dictionary<int, int> GetOptions()
-        {
-            return new Dictionary<int, int>
-            {
-                { 3, 1 },
-                { 4, 3 },
-                { 5, 6 },
-                { 6, 7 },
-                { 7, 6 },
-                { 8, 3 },
-                { 9, 1 }
-            };
         }
     }
 }
