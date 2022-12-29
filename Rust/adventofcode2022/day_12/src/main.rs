1069a2d3280b0987375d63d268af299d173bddf5
Erik van der Boom <evdboom@hotmail.com>
2022-12-29 23:25:29 +0100

    day 12 tas more efficient (still bit slower then c#)

--- Rust/adventofcode2022/day_12/src/main.rs
+++ Rust/adventofcode2022/day_12/src/main.rs
@@ -2,11 +2,6 @@ use std::collections::{HashMap, HashSet, VecDeque};
 use std::fs;
 use std::time::Instant;
 
-struct Node {
-    end: bool,
-    connections: Vec<(usize, usize)>,
-}
-
 fn main() {
     let input = fs::read_to_string("./input.txt").expect("Could not read file");
 
@@ -21,71 +16,87 @@ fn main() {
 }
 
 fn part_one(input: &String) -> i32 {
-    let nodes = get_nodes(input, false);
-
-    distance(&nodes.1[0], &nodes.0, &i32::MAX).unwrap()
+    let nodes = get_nodes(input, false);    
+    distance(&nodes.1[0], &nodes.2, &nodes.0, &HashSet::new()).unwrap()
 }
 
 fn part_two(input: &String) -> i32 {
     let nodes = get_nodes(input, true);
     let mut smallest = i32::MAX;
-    for start in nodes.1 {
-        let distance =distance(&start, &nodes.0, &smallest);
+    let mut dead_ends = HashSet::new();
+    for start in &nodes.1 {
+        let distance = distance(start, &nodes.2, &nodes.0, &dead_ends);
         if distance.is_some() && distance.unwrap() < smallest {
             smallest = distance.unwrap();
-        }        
+        } else {
+            dead_ends.insert((start.0, start.1));
+        }
     }
 
     smallest
 }
 
-fn distance(start: &(usize, usize), nodes: &HashMap<(usize, usize), Node>, cut_off: &i32) -> Option<i32> {
+fn distance(
+    start: &(usize, usize),
+    end: &(usize, usize),
+    nodes: &HashMap<(usize, usize), Vec<(usize, usize)>>,
+    dead_ends: &HashSet<(usize, usize)>
+) -> Option<i32> {
     let mut queue = VecDeque::new();
     let mut visited = HashSet::new();
 
     queue.push_back((0, start));
     visited.insert(start);
 
-    while let Some((distance, node)) = queue.pop_front() {
-        if &distance >= cut_off {
-            return None;
-        }
-        if nodes[node].end {
-            return Some(distance);
-        }
-        for connection in &nodes[node].connections {            
-            if visited.insert(connection) {                
-                queue.push_back((distance + 1, connection));
+    while let Some((distance, node)) = queue.pop_front() {   
+        if dead_ends.contains(node) {
+            continue;
+        }     
+        for connection in &nodes[node] {
+            if connection == end {
+                return Some(distance + 1);
+            }
+
+            if visited.insert(connection) {
+                queue.push_back((distance + 1, connection))
             }
         }
-    }
+    }    
     None
 }
 
-fn get_nodes(input: &String, include_a: bool) -> (HashMap<(usize, usize), Node>, Vec<(usize, usize)>) {
+fn get_nodes(
+    input: &String,
+    include_a: bool,
+) -> (
+    HashMap<(usize, usize), Vec<(usize, usize)>>,
+    Vec<(usize, usize)>,
+    (usize, usize),
+) {
     let mut map = HashMap::new();
-    let mut start = vec![];
     let mut height_map = HashMap::new();
-    let mut height = 0usize;
-    let mut width = 0usize;
+    let mut start = vec![];
+    let mut end = (0usize, 0usize);
+
+    let mut height = 0;
+    let mut width = 0;
+
     for (j, row) in input.lines().enumerate() {
-        if j + 1 > height {
-            height = j + 1;
+        if j > height {
+            height = j;
         }
         for (i, cell) in row.bytes().enumerate() {
-            if i + 1 > width {
-                width = i + 1;
+            if i > width {
+                width = i;
             }
-            if cell == b'S' || (include_a && cell == b'a'){
+            map.insert((i, j), vec![]);
+            if cell == b'S' || (include_a && cell == b'a') {
                 start.push((i, j));
             }
-            map.insert(
-                (i, j),
-                Node {
-                    end: cell == b'E',
-                    connections: vec![],
-                },
-            );
+            if cell == b'E' {
+                end.0 = i;
+                end.1 = j;
+            }
             height_map.insert(
                 (i, j),
                 if cell == b'S' {
@@ -94,29 +105,29 @@ fn get_nodes(input: &String, include_a: bool) -> (HashMap<(usize, usize), Node>,
                     b'z'
                 } else {
                     cell
-                } % 32
-                    - 1,
+                },
             );
         }
     }
-    for j in 0..height {
-        for i in 0..width {
-            let current = map.get_mut(&(i, j)).unwrap();
+
+    for j in 0..=height {
+        for i in 0..=width {
             let value = height_map[&(i, j)];
+            let connections = map.get_mut(&(i, j)).unwrap();
             if j > 0 && height_map[&(i, j - 1)] <= value + 1 {
-                current.connections.push((i, j - 1));
-            }
-            if j < height - 1 && height_map[&(i, j + 1)] <= value + 1 {
-                current.connections.push((i, j + 1));
+                connections.push((i, j - 1));
             }
             if i > 0 && height_map[&(i - 1, j)] <= value + 1 {
-                current.connections.push((i - 1, j));
+                connections.push((i - 1, j));
             }
-            if i < width - 1 && height_map[&(i + 1, j)] <= value + 1 {
-                current.connections.push((i + 1, j));
+            if j < height && height_map[&(i, j + 1)] <= value + 1 {
+                connections.push((i, j + 1));
+            }
+            if i < width && height_map[&(i + 1, j)] <= value + 1 {
+                connections.push((i + 1, j));
             }
         }
     }
 
-    (map, start)
+    (map, start, end)
 }
