d77493ef8467d6d8816bf4a510e7a1c2c6e116bb
Erik van der Boom <evdboom@hotmail.com>
2024-12-21 23:55:23 +0100

    With tip for splitting code in pieces, fixed 2024 day 21 rust

--- Rust/adventofcode2024/day_21/src/main.rs
+++ Rust/adventofcode2024/day_21/src/main.rs
@@ -2,30 +2,30 @@ use std::collections::HashMap;
 use std::fs;
 use std::time::Instant;
 
-static NUMERIC_KEYPAD: [(char, (isize, isize)); 11] = [
-    ('7', (0, 0)),
-    ('8', (1, 0)),
-    ('9', (2, 0)),
-    ('4', (0, 1)),
-    ('5', (1, 1)),
-    ('6', (2, 1)),
-    ('1', (0, 2)),
-    ('2', (1, 2)),
-    ('3', (2, 2)),
-    ('0', (1, 3)),
-    ('A', (2, 3)),
+static NUMERIC_KEYPAD: [(u8, (isize, isize)); 11] = [
+    (b'7', (0, 0)),
+    (b'8', (1, 0)),
+    (b'9', (2, 0)),
+    (b'4', (0, 1)),
+    (b'5', (1, 1)),
+    (b'6', (2, 1)),
+    (b'1', (0, 2)),
+    (b'2', (1, 2)),
+    (b'3', (2, 2)),
+    (b'0', (1, 3)),
+    (b'A', (2, 3)),
 ];
 
-static DIRECTIONAL_KEYPAD: [(char, (isize, isize)); 5] = [
-    ('^', (1, 0)),
-    ('A', (2, 0)),
-    ('<', (0, 1)),
-    ('v', (1, 1)),
-    ('>', (2, 1)),
+static DIRECTIONAL_KEYPAD: [(u8, (isize, isize)); 5] = [
+    (b'^', (1, 0)),
+    (b'A', (2, 0)),
+    (b'<', (0, 1)),
+    (b'v', (1, 1)),
+    (b'>', (2, 1)),
 ];
 
 fn main() {
-    let input = fs::read_to_string("./input_test.txt").expect("Could not read file");
+    let input = fs::read_to_string("./input.txt").expect("Could not read file");
     let start = Instant::now();
     let part_one = process_part_one(&input);
     let duration_one = start.elapsed();
@@ -45,7 +45,7 @@ fn process_part_two(input: &str) -> usize {
 }
 
 fn perform_keypad_presses(input: &str, chain_length: usize) -> usize {
-    let numeric_keypad: HashMap<char, (isize, isize)> = NUMERIC_KEYPAD
+    let numeric_keypad = NUMERIC_KEYPAD
         .iter()
         .map(|&(key, coords)| (key, coords))
         .collect();
@@ -57,63 +57,101 @@ fn perform_keypad_presses(input: &str, chain_length: usize) -> usize {
     let numeric_start: (isize, isize) = (2, 3);
     let directional_start: (isize, isize) = (2, 0);
 
+    let mut cache: HashMap<(Vec<u8>, usize), usize> = HashMap::new();
     let mut result = 0;
     for numeric_code in input.lines() {
+        let mut length = 0;
         let code_value = numeric_code[..numeric_code.len() - 1]
             .parse::<usize>()
             .unwrap();
-        let mut code = get_path(&numeric_code, &numeric_keypad, &numeric_start, 3);
-        for i in 0..chain_length {
-            println!("Step: {}", i);
-            code = get_path(&code, &directional_keypad, &directional_start, 0);
+        let codes = get_paths(
+            &numeric_code.as_bytes().to_vec(),
+            &numeric_keypad,
+            &numeric_start,
+            3,
+        );
+
+        for code in codes.into_iter() {
+            length += get_code_length(
+                code,
+                &directional_keypad,
+                &directional_start,
+                0,
+                chain_length,
+                &mut cache,
+            );
         }
 
-        result += code.len() * code_value;
+        result += length * code_value;
     }
 
     result
 }
 
-fn get_path(
-    code: &str,
-    keypad: &HashMap<char, (isize, isize)>,
+fn get_code_length(
+    code: Vec<u8>,
+    keypad: &HashMap<u8, (isize, isize)>,
     start: &(isize, isize),
     gap_row: isize,
-) -> String {
-    let mut path = String::new();
+    chain_length: usize,
+    cache: &mut HashMap<(Vec<u8>, usize), usize>,
+) -> usize {
+    if chain_length == 0 {
+        return code.len();
+    } else if let Some(cached) = cache.get(&(code.to_vec(), chain_length)) {
+        return *cached;
+    }
+
+    let mut length = 0;
+    let paths = get_paths(&code, keypad, start, gap_row);
+    for path in paths.into_iter() {
+        length += get_code_length(path, keypad, start, gap_row, chain_length - 1, cache);
+    }
+    cache.insert((code, chain_length), length);
+    length
+}
+
+fn get_paths(
+    code: &Vec<u8>,
+    keypad: &HashMap<u8, (isize, isize)>,
+    start: &(isize, isize),
+    gap_row: isize,
+) -> Vec<Vec<u8>> {
+    let mut paths = Vec::new();
     let mut position = start.clone();
-    for c in code.chars() {
+    for c in code {
+        let mut path = vec![];
         let (x, y) = keypad.get(&c).unwrap().clone();
 
         // move left if not on gap row, or if on gap row and not at the edge ('<' per step)
         if (x != 0 || position.1 != gap_row) && x < position.0 {
             for _ in 0..(position.0 - x).abs() {
-                path.push('<');
+                path.push(b'<');
             }
             position.0 = x;
         }
         // move up / down ('^' or 'v' per step) if not on gap column (0), or if on gap column and not at the edge
         if position.0 != 0 || y != gap_row {
             for _ in 0..(position.1 - y).abs() {
-                path.push(if y < position.1 { '^' } else { 'v' });
+                path.push(if y < position.1 { b'^' } else { b'v' });
             }
             position.1 = y;
         }
         // move left / right if still needed ('<' or '>' per step)
         for _ in 0..(position.0 - x).abs() {
-            path.push(if x < position.0 { '<' } else { '>' });
+            path.push(if x < position.0 { b'<' } else { b'>' });
         }
         // move up / down if still needed ('^' or 'v' per step)
         for _ in 0..(position.1 - y).abs() {
-            path.push(if y < position.1 { '^' } else { 'v' });
+            path.push(if y < position.1 { b'^' } else { b'v' });
         }
 
-        path.push('A');
-
+        path.push(b'A');
+        paths.push(path);
         position = (x, y);
     }
 
-    path
+    paths
 }
 
 #[cfg(test)]
@@ -143,6 +181,6 @@ mod tests {
 379A",
         );
 
-        assert_eq!(117440, process_part_one(&input));
+        assert_eq!(154115708116294, process_part_two(&input));
     }
 }
